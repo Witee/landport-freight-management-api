a664610925e42eb8eef008081f85851c73a9e26e
Witee <525942866@qq.com>
2025-11-02 21:38:19 +0800

    fix

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -1,5 +1,10 @@
 export default {
   sequelize: {
+    host: 'host.docker.internal',
+    port: 3306,
+    database: 'landport',
+    username: 'root',
+    password: 'Admin123.',
     // 生产环境统一东八区
     timezone: '+08:00',
     dialectOptions: {
@@ -13,6 +18,14 @@ export default {
         } catch {}
         return next();
       },
+    },
+  },
+  redis: {
+    client: {
+      port: 6379,
+      host: 'host.docker.internal',
+      password: '',
+      db: 0,
     },
   },
 };
